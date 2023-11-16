d0cbd5d3a955f8dfc6c0748d27547b8413d983ab
Bannu Akkabattula <bannu@quanbytech.onmicrosoft.com>
2023-11-16 14:29:20 +0530

    home page updated

--- components/banner.jsx
+++ components/banner.jsx
@@ -4,20 +4,15 @@ const Banner = () =>{
     return(
         <div>
 
-
-
-
-<div >
  
     <div class="relative h-56 overflow-hidden rounded-lg md:h-96">
          <div class=" duration-700 ease-in-out" data-carousel-item>
                <img src="assets/images/banners/banner02.png" class="absolute block w-full h-full w-sm-100 "  alt="..."/>
         </div>    
     </div>
+
+
 </div>
-
-
-        </div>
     )
 }
 
--- components/cards.jsx
+++ components/cards.jsx
@@ -9,32 +9,32 @@ const frequencies = [
 ]
 const tiers = [
   {
-    name: 'Freelancer',
+    name: 'Tiger',
     id: '1',
     href: '#',
     src:'https://assets-in.bmscdn.com/discovery-catalog/events/tr:w-400,h-600,bg-CCCCCC:w-400.0,h-660.0,cm-pad_resize,bg-000000,fo-top:oi-discovery-catalog@@icons@@star-icon-202203010609.png,ox-24,oy-615,ow-29:ote-Ny44LzEwICAxMjEuMUsgVm90ZXM%3D,ots-29,otc-FFFFFF,oy-612,ox-70:q-80/et00313411-vvkjfxrysb-portrait.jpg',
     
   },
   {
-    name: 'Freelancer',
+    name: 'Mangalavaram',
     id: '2',
     href: '#',
     src:'https://assets-in.bmscdn.com/discovery-catalog/events/tr:w-400,h-600,bg-CCCCCC:w-400.0,h-660.0,cm-pad_resize,bg-000000,fo-top:oi-discovery-catalog@@icons@@like_202006280402.png,ox-24,oy-617,ow-29:ote-NDYuNEsgTGlrZXM%3D,ots-29,otc-FFFFFF,oy-612,ox-70:q-80/et00358028-vexszysezf-portrait.jpg',
   },
   {
-    name: 'Startup',
+    name: 'Polimer-2',
     id: '3',
     href: '#',
     src:'https://assets-in.bmscdn.com/discovery-catalog/events/tr:w-400,h-600,bg-CCCCCC:w-400.0,h-660.0,cm-pad_resize,bg-000000,fo-top:oi-discovery-catalog@@icons@@star-icon-202203010609.png,ox-24,oy-615,ow-29:ote-OC44LzEwICAyM0sgVm90ZXM%3D,ots-29,otc-FFFFFF,oy-612,ox-70:q-80/et00372040-dkumbmrkfr-portrait.jpg',
   },
   {
-    name: 'Enterprise',
+    name: 'Jigarathanda',
     id: '4',
     href: '#',
     src:'https://assets-in.bmscdn.com/discovery-catalog/events/tr:w-400,h-600,bg-CCCCCC:w-400.0,h-660.0,cm-pad_resize,bg-000000,fo-top:oi-discovery-catalog@@icons@@star-icon-202203010609.png,ox-24,oy-615,ow-29:ote-OC45LzEwICAzNC45SyBWb3Rlcw%3D%3D,ots-29,otc-FFFFFF,oy-612,ox-70:q-80/et00359702-efjublekkp-portrait.jpg',
   },
   {
-    name: 'Enterprise',
+    name: 'Spark',
     id: '5',
     href: '#',
     src:'https://assets-in.bmscdn.com/discovery-catalog/events/tr:w-400,h-600,bg-CCCCCC:w-400.0,h-660.0,cm-pad_resize,bg-000000,fo-top:oi-discovery-catalog@@icons@@like_202006280402.png,ox-24,oy-617,ow-29:ote-ODAuNUsgTGlrZXM%3D,ots-29,otc-FFFFFF,oy-612,ox-70:q-80/et00366257-rvxmghlrbl-portrait.jpg',
@@ -46,7 +46,7 @@ function classNames(...classes) {
 }
 
 const Cards = () =>{
-    const [frequency, setFrequency] = useState(frequencies[0])
+
     return(
         <div>
                 <div className="bg-gray-900 py-24 sm:py-32">
@@ -96,7 +96,47 @@ const Cards = () =>{
           ))}
         </div>
       </div>
-    </div>
+               </div>
+
+               <div className="bg-white py-2 pb-20">
+      <div className="mx-auto max-w-7xl px-6 lg:px-8">
+        <div className="isolate mx-auto mt-10 grid max-w-md grid-cols-1 gap-8 lg:mx-0 lg:max-w-none lg:grid-cols-5">
+          {tiers.map((tier) => (
+            <div
+              key={"tier.id"}
+              className={classNames(
+                tier.mostPopular ? 'bg-white/5 ring-2 ring-indigo-500' : 'ring-1 ring-dark/10',
+                'rounded-3xl p-8 xl:p-8'
+              )}
+            >
+              <div className="flex items-center justify-between gap-x-4">
+                <h3 id={tier.id} className="text-lg font-semibold leading-8 text-dark">
+                  {tier.name}
+                </h3>
+             
+              </div>
+              <div>
+                <img src={tier.src} alt="" />
+              </div>
+            
+              <a
+                href={tier.href}
+                aria-describedby={tier.id}
+                className={classNames(
+                  tier.mostPopular
+                    ? 'bg-indigo-500 text-white shadow-sm hover:bg-dark-900 focus-visible:outline-indigo-500'
+                    : 'bg-dark/10 text-dark hover:bg-white/20 focus-visible:outline-dark',
+                  'mt-6 block rounded-md py-2 px-3 text-center text-sm font-semibold leading-6 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2'
+                )}
+              >
+                Book Now
+              </a>
+             
+            </div>
+          ))}
+        </div>
+      </div>
+               </div>
         </div>
     )
 }
